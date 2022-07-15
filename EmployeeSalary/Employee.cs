87b59e9c8714422ab597dcaef0b8bfbf42728433
deivisondev <deivisoneditordevideos@gmail.com>
2022-07-15 09:50:35 -0300

    Início

---
+++ EmployeeSalary/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeSalary
+{
+    public class Employee
+    {
+        public string? Name;
+        public double GrossSalary;
+        public double Tax;
+
+        public void IncreaseSalary(double porcentage)
+        {
+            GrossSalary += (GrossSalary * porcentage) / 100;
+        }
+
+        public double NetSalary()
+        {
+            return GrossSalary - Tax;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}, $ {NetSalary().ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
---
+++ EmployeeSalary/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace EmployeeSalary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+
+            Console.Write("Nome: ");
+            employee.Name = Console.ReadLine();
+            Console.Write("Salário bruto: ");
+            employee.GrossSalary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Imposto: ");
+            employee.Tax = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine($"\nFuncionário: {employee}");
+
+            Console.Write("\nDigite a porcentagem para aumentar o salário: ");
+            employee.IncreaseSalary(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+
+            Console.WriteLine($"\nDados atualizados: {employee}");
+        }
+    }
+}
